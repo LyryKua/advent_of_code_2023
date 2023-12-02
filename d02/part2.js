78d63735cbd5489ec41706ebaddbdaf64e2ffc0f
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-02 11:21:11 +0100

    d02:part2

---
+++ d02/part2.js
@@ -0,0 +1,55 @@
+import { example, input } from './input.js'
+import { parseInput } from '../lib/index.js'
+
+/**
+ * @param str {string}
+ * @returns {{red: number, green: number, blue: number}}
+ */
+function getTry(str) {
+  const answer = {
+    red: 0,
+    green: 0,
+    blue: 0,
+  }
+  const cubes = str.split(', ')
+
+  for (const cube of cubes) {
+    const [amount, color] = cube.split(' ')
+    answer[color] = parseInt(amount)
+  }
+
+  return answer
+}
+
+/**
+ * @param input {string}
+ * @return {number}
+ */
+function main(input) {
+  const lines = parseInput(input)
+
+  const parsedLines = lines
+    .map(line => {
+      const [_, cubes] = line.split(': ')
+      return cubes
+    })
+    .map(it => it.split('; '))
+    .map(tries => tries.map(getTry))
+
+  return parsedLines.map((tries, index) => {
+    let maxRed = 0
+    let maxGreen = 0
+    let maxBlue = 0
+    for (const try_ of tries) {
+      maxRed = Math.max(maxRed, try_.red)
+      maxGreen = Math.max(maxGreen, try_.green)
+      maxBlue = Math.max(maxBlue, try_.blue)
+    }
+
+    return maxRed * maxGreen * maxBlue
+  }).reduce((acc, cur) => acc + cur)
+}
+
+const exampleResult = main(example)
+console.log('example:', exampleResult, exampleResult === 2286)
+console.log('answer:', main(input))
