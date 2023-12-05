fad67f03d185efdac9ed66c1c58954d2128d675b
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-05 13:49:37 +0100

    parsed input

--- d05/index.js
+++ d05/index.js
@@ -1,14 +1,49 @@
 import { example } from './input.js'
-import { parseInput } from '../lib/index.js'
+
+/**
+ * @param input {string}
+ * @returns {{seeds: number[], seedToSoilMap: number[][], soilToFertilizerMap: number[][], fertilizerToWaterMap: number[][], waterToLightMap: number[][], lightToTemperatureMap: number[][], temperatureToHumidityMap: number[][], humidityToLocationMap: number[][]}}
+ */
+function parseSeeds(input) {
+  const [seeds, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap] = input.trim().split('\n\n')
+
+  return {
+    seeds: seeds.replaceAll('seeds: ', '').trim().split(' ').map(it => parseInt(it)),
+    seedToSoilMap: seedToSoilMap.replaceAll('seed-to-soil map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+    soilToFertilizerMap: soilToFertilizerMap.replaceAll('soil-to-fertilizer map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+    fertilizerToWaterMap: fertilizerToWaterMap.replaceAll('fertilizer-to-water map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+    waterToLightMap: waterToLightMap.replaceAll('water-to-light map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+    lightToTemperatureMap: lightToTemperatureMap.replaceAll('light-to-temperature map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+    temperatureToHumidityMap: temperatureToHumidityMap.replaceAll('temperature-to-humidity map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+    humidityToLocationMap: humidityToLocationMap.replaceAll('humidity-to-location map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
+  }
+}
 
 /**
  * @param input {string}
  * @returns {number}
  */
 function part1(input) {
-  const lines = parseInput(input)
+  const {
+    seeds,
+    seedToSoilMap,
+    soilToFertilizerMap,
+    fertilizerToWaterMap,
+    waterToLightMap,
+    lightToTemperatureMap,
+    temperatureToHumidityMap,
+    humidityToLocationMap,
+  } = parseSeeds(input)
 
-  return lines.length
+  console.log('seeds:', seeds)
+  console.log('seedToSoilMap:', seedToSoilMap)
+  console.log('soilToFertilizerMap:', soilToFertilizerMap)
+  console.log('fertilizerToWaterMap:', fertilizerToWaterMap)
+  console.log('waterToLightMap:', waterToLightMap)
+  console.log('lightToTemperatureMap:', lightToTemperatureMap)
+  console.log('temperatureToHumidityMap:', temperatureToHumidityMap)
+  console.log('humidityToLocationMap:', humidityToLocationMap)
+  return 0
 }
 
 console.log('--- Day 5: If You Give A Seed A Fertilizer ---')
