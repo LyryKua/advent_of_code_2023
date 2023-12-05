f64616e97c469d2a7ad35f44345a62ec3b6bc365
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-05 20:16:54 +0100

    d05:part2

--- d05/index.js
+++ d05/index.js
@@ -4,35 +4,11 @@ import { example, input } from './input.js'
  * @param input {string}
  * @returns {{seeds: number[], seedToSoilMap: number[][], soilToFertilizerMap: number[][], fertilizerToWaterMap: number[][], waterToLightMap: number[][], lightToTemperatureMap: number[][], temperatureToHumidityMap: number[][], humidityToLocationMap: number[][]}}
  */
-function parseSeedsPart1(input) {
+function parseSeeds(input) {
   const [seeds, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap] = input.trim().split('\n\n')
 
   return {
     seeds: seeds.replaceAll('seeds: ', '').trim().split(' ').map(it => parseInt(it)),
-    seedToSoilMap: seedToSoilMap.replaceAll('seed-to-soil map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-    soilToFertilizerMap: soilToFertilizerMap.replaceAll('soil-to-fertilizer map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-    fertilizerToWaterMap: fertilizerToWaterMap.replaceAll('fertilizer-to-water map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-    waterToLightMap: waterToLightMap.replaceAll('water-to-light map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-    lightToTemperatureMap: lightToTemperatureMap.replaceAll('light-to-temperature map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-    temperatureToHumidityMap: temperatureToHumidityMap.replaceAll('temperature-to-humidity map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-    humidityToLocationMap: humidityToLocationMap.replaceAll('humidity-to-location map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
-  }
-}
-
-/**
- * @param input {string}
- * @returns {{seeds: number[], seedToSoilMap: number[][], soilToFertilizerMap: number[][], fertilizerToWaterMap: number[][], waterToLightMap: number[][], lightToTemperatureMap: number[][], temperatureToHumidityMap: number[][], humidityToLocationMap: number[][]}}
- */
-function parseSeedsPart2(input) {
-  const [seeds, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap] = input.trim().split('\n\n')
-
-  const [seed1, range1, seed2, range2] = seeds.replaceAll('seeds: ', '').trim().split(' ').map(it => parseInt(it))
-
-  return {
-    seeds: [
-      ...Array(range1).fill(42).map((_, i) => seed1 + i),
-      ...Array(range2).fill(42).map((_, i) => seed2 + i),
-    ],
     seedToSoilMap: seedToSoilMap.replaceAll('seed-to-soil map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
     soilToFertilizerMap: soilToFertilizerMap.replaceAll('soil-to-fertilizer map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
     fertilizerToWaterMap: fertilizerToWaterMap.replaceAll('fertilizer-to-water map:\n', '').trim().split('\n').map(row => row.trim().split(' ').map(it => parseInt(it))),
@@ -67,9 +43,20 @@ function originToTarget(x, target, origin, range) {
  * @returns {number}
  */
 function oneToAnother(x, map) {
+  /*
   const soils = map.map(row => originToTarget(x, ...row)).filter(it => it !== x)
 
-  return soils.length === 1 ? soils[0] : x
+  let y = soils.length === 1 ? soils[0] : x
+   */
+  let y = x
+  for (const row of map) {
+    const target = originToTarget(x, ...row)
+    if (y !== target) {
+      return target
+    }
+  }
+
+  return y
 }
 
 /**
@@ -86,7 +73,7 @@ function part1(input) {
     lightToTemperatureMap,
     temperatureToHumidityMap,
     humidityToLocationMap,
-  } = parseSeedsPart1(input)
+  } = parseSeeds(input)
 
   const soils = seeds.map(seed => oneToAnother(seed, seedToSoilMap))
   const fertilizers = soils.map(soil => oneToAnother(soil, soilToFertilizerMap))
@@ -104,6 +91,7 @@ function part1(input) {
  * @returns {number}
  */
 function part2(input) {
+  let min = 9007199254740992
   const {
     seeds,
     seedToSoilMap,
@@ -113,17 +101,27 @@ function part2(input) {
     lightToTemperatureMap,
     temperatureToHumidityMap,
     humidityToLocationMap,
-  } = parseSeedsPart2(input)
+  } = parseSeeds(input)
 
-  const soils = seeds.map(seed => oneToAnother(seed, seedToSoilMap))
-  const fertilizers = soils.map(soil => oneToAnother(soil, soilToFertilizerMap))
-  const waters = fertilizers.map(fertilizer => oneToAnother(fertilizer, fertilizerToWaterMap))
-  const lights = waters.map(water => oneToAnother(water, waterToLightMap))
-  const temperatures = lights.map(light => oneToAnother(light, lightToTemperatureMap))
-  const humidities = temperatures.map(temperature => oneToAnother(temperature, temperatureToHumidityMap))
-  const locations = humidities.map(humidity => oneToAnother(humidity, humidityToLocationMap))
+  for (let i = 0; i < seeds.length; i += 2) {
+    console.log('i:', i, '-', new Date())
+    const start = seeds[i]
+    const end = start + seeds[i + 1]
+    for (let seed = start; seed < end; seed++) {
+      const soil = oneToAnother(seed, seedToSoilMap)
+      const fertilizer = oneToAnother(soil, soilToFertilizerMap)
+      const water = oneToAnother(fertilizer, fertilizerToWaterMap)
+      const light = oneToAnother(water, waterToLightMap)
+      const temperature = oneToAnother(light, lightToTemperatureMap)
+      const humidity = oneToAnother(temperature, temperatureToHumidityMap)
+      const location = oneToAnother(humidity, humidityToLocationMap)
+      if (location < min) {
+        min = location
+      }
+    }
+  }
 
-  return Math.min(...locations)
+  return min
 }
 
 console.log('--- Day 5: If You Give A Seed A Fertilizer ---')
