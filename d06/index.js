caedbcdde5ba08132dbf7c2d453020048568bb88
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-06 08:55:19 +0100

    d06:part1

--- d06/index.js
+++ d06/index.js
@@ -1,5 +1,26 @@
 import { example, input } from './input.js'
-import { parseInput } from '../lib/index.js'
+
+/**
+ * @param input {string}
+ * @returns {{time: number, distance: number}[]}
+ */
+function parseInput(input) {
+  const [times, distances] = input.trim().split('\n').map(line => {
+    const [_, numbers] = line.split(':')
+    return numbers.trim().split(/\s+/).map(Number)
+  })
+
+  return times.map((time, i) => ({ time, distance: distances[i] }))
+}
+
+/**
+ * @param params {{time: number, distance: number}}
+ * @param x {number}
+ * @returns {number}
+ */
+function calculateDistance(x, { time, distance }) {
+  return x * (time - x)
+}
 
 /**
  * @param input {string}
@@ -7,14 +28,26 @@ import { parseInput } from '../lib/index.js'
  */
 function part1(input) {
   const lines = parseInput(input)
-  return lines.length
+  let answer = 1
+  for (const line of lines) {
+    let winCounter = 0
+    for (let i = 0; i < line.time; i++) {
+      const distance = calculateDistance(i, line)
+      if (distance > line.distance) {
+        winCounter += 1
+      }
+    }
+    answer *= winCounter
+  }
+
+  return answer
 }
 
 console.log('--- Day 6: Wait For It ---')
 console.log('\npart1:')
 const examplePart1Result = part1(example)
-console.log('example:', examplePart1Result, examplePart1Result === 35)
-// console.log('answer:', part1(input))
+console.log('example:', examplePart1Result, examplePart1Result === 288)
+console.log('answer:', part1(input))
 
 // console.log('\npart2:')
 // const examplePart2Result = part2(example)
