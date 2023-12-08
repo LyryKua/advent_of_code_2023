72461752a8e631af47d18c8b11b5dad522657f28
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-08 13:30:58 +0100

    d08:part

--- d08/index.js
+++ d08/index.js
@@ -1,19 +1,47 @@
-import { example1, input } from './input.js'
+import { example1, example2, input } from './input.js'
+
+/**
+ * @param input {string}
+ * @returns {{instructions: string, coordinates: Object<string, {L: string, R: string}>}}
+ */
+function parseInput(input) {
+  const [instructions, _, ...coordinates] = input.trim().split('\n')
+
+  return {
+    instructions,
+    coordinates: coordinates.reduce((acc, line) => {
+      const [key, LR] = line.split(' = ')
+      const [L, R] = LR.replace(/[()]/g, '').split(', ')
+      return { ...acc, [key]: { L, R } }
+    }, {}),
+  }
+}
 
 /**
  * @param input {string}
  * @returns {number}
  */
 function part1(input) {
-  return input.length
+  const { instructions, coordinates } = parseInput(input)
+  let start = 'AAA'
+  let answer = 0
+  while (start !== 'ZZZ') {
+    const instruction = instructions[answer % instructions.length]
+    start = coordinates[start][instruction]
+    answer += 1
+  }
+
+  return answer
 }
 
 console.log('--- Day 8: Haunted Wasteland ---')
 console.log('\npart1:')
 const example1Part1Result = part1(example1)
-console.log('example1:', example1Part1Result, example1Part1Result === -1)
+console.log('example1:', example1Part1Result, example1Part1Result === 2)
+const example2Part1Result = part1(example2)
+console.log('example2:', example2Part1Result, example2Part1Result === 6)
 const part1Result = part1(input)
-console.log('answer:', part1Result)
+console.log('answer:', part1Result part1Result === 17621)
 
 /*
 console.log('\npart2:')
