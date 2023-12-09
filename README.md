eba6c2601c3a57f2dc24fabfeeb9549da2aa2e22
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-09 11:20:45 +0100

    d09:part1

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 
 ## Solutions
 
+[//]: # (https://badgen.net/badge/XX/%E2%98%85%E2%98%86/yellow)
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](./d01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](./d02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](./d03)
@@ -22,7 +23,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](./d06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](./d07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](./d08)
-[![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)](./d09)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](./d09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
--- d09/index.js
+++ d09/index.js
@@ -1,24 +1,71 @@
 import { example, input } from './input.js'
-import lcm from 'compute-lcm'
+import { parseInput } from '../lib/index.js'
+
+/**
+ * @param arr {number[]}
+ * @returns {number[]}
+ */
+function getDeltas(arr) {
+  const deltas = []
+  for (let i = arr.length - 1; i > 0; i--) {
+    deltas.unshift(arr[i] - arr[i - 1])
+  }
+  return deltas
+}
+
+/**
+ * @param arr {number[]}
+ * @returns{number[][]}
+ */
+function getPyramid(arr) {
+  const pyramid = [arr]
+  for (let i = 0; i < arr.length - 2; i++) {
+    pyramid.push(getDeltas(pyramid[i]))
+  }
+  return pyramid
+}
+
+/**
+ * @param pyramid {number[][]}
+ * @returns {number}
+ */
+function getPrediction(pyramid) {
+  let prediction = 0
+  for (let i = pyramid.length - 1; i >= 0; i--) {
+    prediction = prediction + pyramid[i][pyramid[i].length - 1]
+  }
+
+  return prediction
+}
 
 /**
  * @param input {string}
  * @returns {number}
  */
 function main(input) {
-  return input.length
+  let answer = 0
+  const lines = parseInput(input)
+  const numbers = lines.map(line => line.split(' ').map(it => parseInt(it)))
+
+  for (const number of numbers) {
+    const pyramid = getPyramid(number)
+    answer += getPrediction(pyramid)
+  }
+
+  return answer
 }
 
 console.log('--- Day 9: Mirage Maintenance ---')
 
 console.log('\npart1:')
-const example1Part1Result = main(example)
-console.log('example:', example1Part1Result, example1Part1Result === 2)
+const examplePart1Result = main(example)
+console.log('example:', examplePart1Result, examplePart1Result === 114)
 const part1Result = main(input)
-console.log('answer:', part1Result, part1Result === 17621)
-
+console.log('answer:', part1Result)
+/*
 console.log('\npart2:')
 const example3Part2Result = main(example)
 console.log('example:', example3Part2Result, example3Part2Result === 6)
 const part2Result = main(input)
 console.log('answer:', part2Result, part2Result === 20685524831999)
+ */
