306913425ea3847af5519b9f58afa085757fea31
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-09 11:36:07 +0100

    d09:part2

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](./d06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](./d07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](./d08)
-[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](./d09)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](./d09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
--- d09/index.js
+++ d09/index.js
@@ -27,12 +27,15 @@ function getPyramid(arr) {
 
 /**
  * @param pyramid {number[][]}
+ * @param part {number}
  * @returns {number}
  */
-function getPrediction(pyramid) {
+function getPrediction(pyramid, part = 1) {
   let prediction = 0
   for (let i = pyramid.length - 1; i >= 0; i--) {
-    prediction = prediction + pyramid[i][pyramid[i].length - 1]
+    prediction = part === 1
+      ? prediction + pyramid[i][pyramid[i].length - 1]
+      : pyramid[i][0] - prediction
   }
 
   return prediction
@@ -40,16 +43,17 @@ function getPrediction(pyramid) {
 
 /**
  * @param input {string}
+ * @param part {number}
  * @returns {number}
  */
-function main(input) {
+function main(input, part) {
   let answer = 0
   const lines = parseInput(input)
   const numbers = lines.map(line => line.split(' ').map(it => parseInt(it)))
 
   for (const number of numbers) {
     const pyramid = getPyramid(number)
-    answer += getPrediction(pyramid)
+    answer += getPrediction(pyramid, part)
   }
 
   return answer
@@ -58,14 +62,13 @@ function main(input) {
 console.log('--- Day 9: Mirage Maintenance ---')
 
 console.log('\npart1:')
-const examplePart1Result = main(example)
+const examplePart1Result = main(example, 1)
 console.log('example:', examplePart1Result, examplePart1Result === 114)
-const part1Result = main(input)
-console.log('answer:', part1Result)
-/*
+const part1Result = main(input, 1)
+console.log('answer:', part1Result, part1Result === 1939607039)
+
 console.log('\npart2:')
-const example3Part2Result = main(example)
-console.log('example:', example3Part2Result, example3Part2Result === 6)
-const part2Result = main(input)
-console.log('answer:', part2Result, part2Result === 20685524831999)
- */
+const examplePart2Result = main(example, 2)
+console.log('example:', examplePart2Result, examplePart2Result === 2)
+const part2Result = main(input, 2)
+console.log('answer:', part2Result, part2Result === 1041)
