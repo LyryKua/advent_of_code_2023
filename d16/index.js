b54f9bd250eb61c2efc6dab374a606936ecda689
Kyrylo Hrecheniuk <kyryloh@wix.com>
2023-12-16 19:55:03 +0100

    save part

--- d16/index.js
+++ d16/index.js
@@ -7,10 +7,89 @@ const NAME = `\n\n--- Day 16: The Floor Will Be Lava ---`
 
 /**
  * @param input {string}
- * @returns {string[][]}
+ * @returns {{char: string, visited: boolean}[][]}
  */
 function parseInput(input) {
-  return input.trim().split('\n').map(it => it.split(''))
+  return input.trim().split('\n').map(it => it.split('').map(it => ({ char: it, visited: false })))
+}
+
+/**
+ * @param dot {{x: number, y: number}}
+ * @param map {{char: string, visited: boolean}[][]}
+ * @returns {boolean}
+ */
+function isInside({ x, y }, map) {
+  return y >= 0 && y < map.length && x >= 0 && x < map[y].length
+}
+
+/**
+ * @param dot {{x: number, y: number, direction: string}}
+ * @returns {{x: number, y: number, direction: string}}
+ */
+function getNewStart({ x, y, direction }) {
+  if (direction === 'right') return { x: x + 1, y, direction }
+  if (direction === 'down') return { x, y: y + 1, direction }
+  if (direction === 'left') return { x: x - 1, y, direction }
+  return { x, y: y - 1, direction }
+}
+
+/**
+ * @param dot{{x: number, y: number, direction: string}}
+ * @param map {{char: string, visited: boolean}[][]}
+ * @returns {boolean}
+ */
+function isVerticalSplit({ x, y, direction }, map) {
+  const { char } = map[y][x]
+  if (char === '|') return direction === 'right' || direction === 'left'
+  return false
+}
+
+/**
+ * @param dot{{x: number, y: number, direction: string}}
+ * @param map {{char: string, visited: boolean}[][]}
+ * @returns {boolean}
+ */
+function isHorizontalSplit({ x, y, direction }, map) {
+  const { char } = map[y][x]
+  if (char === '-') return direction === 'down' || direction === 'up'
+  return false
+}
+
+/**
+ * @param start {{x: number, y: number, direction: string}}
+ * @param map {{char: string, visited: boolean}[][]}
+ */
+function traceMap(start, map) {
+  let newStart = { ...start }
+  while (isInside(newStart, map)) {
+    console.log(newStart)
+    const { char, visited } = map[newStart.y][newStart.x]
+    if (visited && char !== '/' && char !== '\\') return
+    map[newStart.y][newStart.x].visited = true
+    if (isVerticalSplit(newStart, map)) {
+      traceMap(getNewStart({ ...newStart, direction: 'up' }), map)
+      traceMap(getNewStart({ ...newStart, direction: 'down' }), map)
+      return
+    } else if (isHorizontalSplit(newStart, map)) {
+      traceMap(getNewStart({ ...newStart, direction: 'left' }), map)
+      traceMap(getNewStart({ ...newStart, direction: 'right' }), map)
+      return
+    } else if ((char === '/' && newStart.direction === 'right')
+      || (char === '\\' && newStart.direction === 'left')) {
+      newStart = getNewStart({ ...newStart, direction: 'up' })
+    } else if ((char === '/' && newStart.direction === 'left')
+      || (char === '\\' && newStart.direction === 'right')) {
+      newStart = getNewStart({ ...newStart, direction: 'down' })
+    } else if ((char === '/' && newStart.direction === 'up')
+      || (char === '\\' && newStart.direction === 'down')) {
+      newStart = getNewStart({ ...newStart, direction: 'right' })
+    } else if ((char === '/' && newStart.direction === 'down')
+      || (char === '\\' && newStart.direction === 'up')) {
+      newStart = getNewStart({ ...newStart, direction: 'left' })
+    } else {
+      newStart = getNewStart(newStart)
+    }
+  }
 }
 
 /**
@@ -20,6 +99,9 @@ function parseInput(input) {
 function main(input) {
   let ans = 0
   const map = parseInput(input)
+  const start = { x: 0, y: 0, direction: 'right' }
+  // const start = { x: 4, y: 7, direction: 'right' }
+  traceMap(start, map)
 
   return ans
 }
